ea6c5a9c8584adc0ead4a9c56652fac68e79e9b1
Velibor Vasiljevic <v.vasiljevic@vegait.rs>
2022-06-15 17:00:58 +0200

    spajanje

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from util import add_artists_to_db, add_tracks_to_db
+# from util import add_artists_to_db, add_tracks_to_db
+from util import make_advanced_db
 
 
 if __name__ == "__main__":
     # add_artists_to_db()
-    add_tracks_to_db()
+    # add_tracks_to_db()
+    make_advanced_db()
--- old_util.py
+++ old_util.py
@@ -12,18 +12,26 @@ def make_advanced_db(file_name = "data/tracks.csv", url = 'mongodb://localhost:2
     with open(file_name, 'r', encoding = 'cp850') as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
-            artists = []
-            for artist_id in row["id_artists"]:
-                if len(ast.literal_eval(row['id_artists'])) > 0:
-                    artist = get_artist_by_id(artist_id)
-                    artists.append(artist)
-                    break
-            row['artists'] = artists
             track = get_track(row)
-            print(track)
+            # print(track)
             tracks.append(track)
 
     db['tracks'].insert_many(tracks)
+
+
+
+def add_artists_to_db(file_name = "data/artists.csv", url = 'mongodb://localhost:27017/', db_name = 'sbp'):
+        client = pymongo.MongoClient(url)
+        db = client[db_name]
+        artists = []
+        with open(file_name, 'r', encoding = 'cp850') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                artist = get_artist(row)
+                # print(artist)
+                artists.append(artist)
+
+        db['artists'].insert_many(artists)
 
 
 def get_artist(row) -> dict:
@@ -50,30 +58,15 @@ def add_tracks_to_db(file_name = "data/tracks.csv", url = 'mongodb://localhost:2
         db['tracks'].insert_many(tracks)
 
 
-def get_artist_by_id(id: str) -> dict:
-    # for artist in artists:
-    #     if artist['id'] == id:
-    #         return artist
-    with open('data/artists.csv', 'r', encoding = 'cp850') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            if row['id'] == id:
-                return {
-                    '_id': row['id'],
-                    'followers': int(float(row['followers'])) if row['followers'] else 0,
-                    'genres': ast.literal_eval(row['genres']),
-                    'name': row['name'],
-                    'popularity': int(row['popularity']),
-                }
-
-    # if row['id'] == id:
-    #     return {
-    #         '_id': row['id'],
-    #         'followers': int(float(row['followers'])) if row['followers'] else 0,
-    #         'genres': ast.literal_eval(row['genres']),
-    #         'name': row['name'],
-    #         'popularity': int(row['popularity']),
-    #     }
+def get_artist_by_id(id: str, row) -> dict:
+    if row['id'] == id:
+        return {
+            '_id': row['id'],
+            'followers': int(float(row['followers'])) if row['followers'] else 0,
+            'genres': ast.literal_eval(row['genres']),
+            'name': row['name'],
+            'popularity': int(row['popularity']),
+        }
 
 
 
@@ -84,8 +77,8 @@ def get_track(row) -> dict:
         'popularity': int(row['popularity']),
         'duration_ms': int(row['duration_ms']),
         'explicit': False if int(row['explicit']) == 0 else True,
-        'artists': row['artists'],
-        # 'id_artists': ast.literal_eval(row['id_artists']),
+        'artists': ast.literal_eval(row['artists']),
+        'id_artists': ast.literal_eval(row['id_artists']),
         'release_date': parser.parse(row['release_date']+"-01-01" if len(row['release_date']) < 5 else row['release_date']),
         'danceability': float(row['danceability']),
         'energy': float(row['energy']),
